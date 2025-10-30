e621d95a21b04e3a09f16ad353a5e7e1692f8c7c
nguyenthephung <136545799+nguyenthephung@users.noreply.github.com>
2025-10-30 17:19:15 +0700

    fix: rename patch to run

--- backend/run-auth.bat
+++ backend/run-auth.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "d:\21126089(2)\RescueNet\demo\auth-service"
+cd /d "d:\21126089(2)\RescueNet\backend\auth-service"
 echo AUTH SERVICE - Port 8080
 .\mvnw.cmd spring-boot:run
 pause
--- backend/run-gateway.bat
+++ backend/run-gateway.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "d:\21126089(2)\RescueNet\demo\api-gateway"
+cd /d "d:\21126089(2)\RescueNet\backend\api-gateway"
 echo API GATEWAY - Port 8888
 .\mvnw.cmd spring-boot:run
 pause
